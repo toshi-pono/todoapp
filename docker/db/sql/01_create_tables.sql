d8d3ed6eb9e47860840cd59f743e65f9b8610786
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2022-11-20 05:33:43 +0900

    ✨ DBスキーマを修正

--- docker/db/sql/01_create_tables.sql
+++ docker/db/sql/01_create_tables.sql
@@ -2,9 +2,27 @@
 DROP TABLE IF EXISTS `tasks`;
 
 CREATE TABLE `tasks` (
-    `id` bigint(20) NOT NULL AUTO_INCREMENT,
+    `id` char(36) PRIMARY KEY,
     `title` varchar(50) NOT NULL,
+    `description` varchar(255) NOT NULL DEFAULT '',
     `is_done` boolean NOT NULL DEFAULT b'0',
-    `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (`id`)
+    `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) DEFAULT CHARSET=utf8mb4;
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+    `id` char(36) PRIMARY KEY,
+    `name`       varchar(50) NOT NULL UNIQUE,
+    `password`   binary(32) NOT NULL,
+    `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) DEFAULT CHARSET=utf8mb4;
+
+DROP TABLE IF EXISTS `ownership`;
+ 
+CREATE TABLE `ownership` (
+    `user_id` char(36) NOT NULL,
+    `task_id` char(36) NOT NULL,
+    PRIMARY KEY (`user_id`, `task_id`)
 ) DEFAULT CHARSET=utf8mb4;
