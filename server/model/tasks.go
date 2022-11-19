61c007271f113821af08691d745b5ab271590839
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2022-11-20 06:20:38 +0900

    🎉 task周りのDB操作を追加

--- server/model/tasks.go
+++ server/model/tasks.go
@@ -1,4 +1,70 @@
 package model
 
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
 type TasksRepository interface {
+	GetTask(taskId uuid.UUID) (*Task, error)
+	GetTasks(limit int, offset int) ([]Task, error)
+	SearchTasks(limit int, offset int, query SearchTaskArgs) ([]Task, error)
+	CreateTask(args CreateTaskArgs) error
+}
+
+type Task struct {
+	ID          uuid.UUID `db:"id"`
+	Title       string    `db:"title"`
+	Description string    `db:"description"`
+	IsDone      bool      `db:"is_done"`
+	CreatedAt   time.Time `db:"created_at"`
+}
+
+type CreateTaskArgs struct {
+	Title       string
+	Description string
+}
+
+type SearchTaskArgs struct {
+	Title string
+}
+
+// GetTask Idからタスクを取得する
+func (repo *SqlxRepository) GetTask(taskId uuid.UUID) (*Task, error) {
+	var task Task
+	err := repo.db.Get(&task, "SELECT * FROM tasks WHERE id = ?", taskId)
+	if err != nil {
+		return nil, err
+	}
+	return &task, nil
+}
+
+// GetTasks タスクを取得する
+func (repo *SqlxRepository) GetTasks(limit int, offset int) ([]Task, error) {
+	var tasks []Task
+	err := repo.db.Select(&tasks, "SELECT * FROM tasks LIMIT ? OFFSET ?", limit, offset)
+	if err != nil {
+		return nil, err
+	}
+	return tasks, nil
+}
+
+// SearchTasks タスクを検索する
+func (repo *SqlxRepository) SearchTasks(limit int, offset int, query SearchTaskArgs) ([]Task, error) {
+	var tasks []Task
+	err := repo.db.Select(&tasks, "SELECT * FROM tasks WHERE title LIKE ? LIMIT ? OFFSET ?", "%"+query.Title+"%", limit, offset)
+	if err != nil {
+		return nil, err
+	}
+	return tasks, nil
+}
+
+// CreateTask タスクを作成する
+func (repo *SqlxRepository) CreateTask(args CreateTaskArgs) error {
+	_, err := repo.db.Exec("INSERT INTO tasks (title, description) VALUES (?, ?)", args.Title, args.Description)
+	if err != nil {
+		return err
+	}
+	return nil
 }
