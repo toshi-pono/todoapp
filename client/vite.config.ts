f27934f5a8eb93583cf124ff4917d7b6bf3b47dc
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2022-11-21 03:39:18 +0900

    ⚙️ 絶対パスを設定

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,15 @@
-import solid from "solid-start/vite";
-import { defineConfig } from "vite";
+import solid from 'solid-start/vite'
+import { defineConfig } from 'vite'
+
+import path from 'path'
+
+const srcPath = path.resolve(__dirname, 'src').replace(/\\/g, '/')
 
 export default defineConfig({
   plugins: [solid()],
-});
+  resolve: {
+    alias: {
+      '/@': srcPath,
+    },
+  },
+})
