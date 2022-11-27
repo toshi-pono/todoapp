26d53c7b3364bfbc50a062252fd96a3098bc9e90
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2022-11-28 06:40:58 +0900

    🌱 convert to react

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,7 @@
-import solid from 'solid-start/vite'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-import path from 'path'
-
-const srcPath = path.resolve(__dirname, 'src').replace(/\\/g, '/')
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [solid()],
-  resolve: {
-    alias: {
-      '/@': srcPath,
-    },
-  },
+  plugins: [react()]
 })
